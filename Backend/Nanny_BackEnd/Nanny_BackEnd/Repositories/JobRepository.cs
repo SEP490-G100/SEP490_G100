efac918b55b2bc2d20e8f6f42a104476d52c4bb8
MyLTH <lethemi436@gmail.com>
2026-03-19 00:34:08 +0700

    FIX/Conflict

--- Backend/Nanny_BackEnd/Nanny_BackEnd/Repositories/JobRepository.cs
+++ Backend/Nanny_BackEnd/Nanny_BackEnd/Repositories/JobRepository.cs
@@ -12,7 +12,7 @@ public class JobRepository
 
     public JobRepository(Sep490NannyDbContext db) => _db = db;
 
-
+    public IQueryable<JobPosting> GetQuery() => _db.JobPostings.AsQueryable();
     public async Task<List<JobPosting>> searchJobPosting(
         SearchJobRequest filters,
         Guid? currentUserId = null,
